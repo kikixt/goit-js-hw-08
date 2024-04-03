53595c1add140df28e3dcc6275032b5267042daf
kikixt <kikixtt44@gmail.com>
2024-04-03 20:03:14 +0300

    виконання всіх завдань

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+import throttle from 'lodash.throttle';
+
+const feedbackForm = document.querySelector('.feedback-form');
+const emailInput = feedbackForm.querySelector('input[name="email"]');
+const messageInput = feedbackForm.querySelector('textarea[name="message"]');
+
+const saveFormStateToLocalStorage = throttle(() => {
+    const formState = {
+        email: emailInput.value,
+        message: messageInput.value
+    };
+    localStorage.setItem('feedback-form-state', JSON.stringify(formState));
+}, 500);
+
+feedbackForm.addEventListener('input', saveFormStateToLocalStorage);
+
+window.addEventListener('DOMContentLoaded', () => {
+    const storedFormState = localStorage.getItem('feedback-form-state');
+    if (storedFormState) {
+        const { email, message } = JSON.parse(storedFormState);
+        emailInput.value = email;
+        messageInput.value = message;
+    }
+});
+
+feedbackForm.addEventListener('submit', event => {
+    event.preventDefault();
+
+    localStorage.removeItem('feedback-form-state');
+    emailInput.value = '';
+    messageInput.value = '';
+
+    console.log({
+        email: emailInput.value,
+        message: messageInput.value
+    });
+});
 
